619cffbb2f01ba8a537c6b57efca0f9ca89c18af
stinojones <stinobuddy@gmail.com>
2024-09-28 19:03:27 -0600

    dark mode added

---
+++ week01/mission.js
@@ -0,0 +1,16 @@
+const select = document.querySelector("#mySelect");
+const footerImage = document.getElementById("footerImage"); // Select the img element
+
+select.addEventListener("change", function () {
+  changeTheme(select.value);
+});
+
+function changeTheme(output) {
+  if (output === "dark") {
+    document.body.classList.add("dark"); // Add dark class to body
+    footerImage.src = "/byui-logo-dark.png"; // Change to dark theme image
+  } else {
+    document.body.classList.remove("dark"); // Remove dark class from body
+    footerImage.src = "/byui-logo.webp"; // Change back to light theme image
+  }
+}
