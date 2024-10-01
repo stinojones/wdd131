db24148178b7dcb1b40dd5f29e741e3ad495cc17
stinojones <stinobuddy@gmail.com>
2024-10-01 15:00:58 -0600

    onload window

--- week01/mission.js
+++ week01/mission.js
@@ -1,16 +1,18 @@
-const select = document.querySelector("#mySelect");
-const footerImage = document.getElementById("footerImage"); // Select the img element
+window.onload = function () {
+  const select = document.querySelector("#mySelect");
+  const footerImage = document.getElementById("footerImage"); // Select the img element
 
-select.addEventListener("change", function () {
-  changeTheme(select.value);
-});
+  select.addEventListener("change", function () {
+    changeTheme(select.value);
+  });
 
-function changeTheme(output) {
-  if (output === "dark") {
-    document.body.classList.add("dark"); // Add dark class to body
-    footerImage.src = "img/byui-logo-dark.png"; // Change to dark theme image
-  } else {
-    document.body.classList.remove("dark"); // Remove dark class from body
-    footerImage.src = "img/byui-logo.webp"; // Change back to light theme image
+  function changeTheme(output) {
+    if (output === "dark") {
+      document.body.classList.add("dark"); // Add dark class to body
+      footerImage.src = "img/byui-logo-dark.png"; // Change to dark theme image
+    } else {
+      document.body.classList.remove("dark"); // Remove dark class from body
+      footerImage.src = "img/byui-logo.webp"; // Change back to light theme image
+    }
   }
-}
+};
